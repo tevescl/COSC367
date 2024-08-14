1724009c0ce070a85cd1e5a28986948efa863676
Claire <102723844+tevescl@users.noreply.github.com>
2024-08-14 20:21:53 +1200

    final Q1 RoutingGraph.py

--- A* assignment 1/RoutingGraph.py
+++ A* assignment 1/RoutingGraph.py
@@ -1,59 +1,64 @@
 from search import *
 import math
-#draw graph of each agent
-
 
 class RoutingGraph(Graph):
     def __init__(self, map_str):
         self.map = [list(line) for line in map_str.strip().split('\n')]
         self.rows = len(self.map)
         self.cols = len(self.map[0])
-        print (self.rows,self.cols)
-        self.agents = {}
+        self.agents = {}#key = location, value: fuel capacity 
         self.goals = set()
         self.portals = []
         
-        for r, row in enumerate(self.map):
-            print (r,row)
-            for c, char in enumerate(row):
-                if char == 'G':
-                    self.goals.add((r, c))
-                elif char == 'S':
-                    self.agents[(r, c)] = math.inf
-                elif char.isdigit():
-                    self.agents[(r, c)] = int(char)
-                elif char == 'P':
-                    self.portals.append((r, c))
-    
+        for row_num, row in enumerate(self.map):
+            for col_num, cell_char in enumerate(row):
+                if cell_char == 'G':
+                    self.goals.add((row_num, col_num))
+                elif cell_char == 'S':#dont need fuel
+                    self.agents[(row_num, col_num)] = math.inf
+                elif cell_char.isdigit():#agent
+                    self.agents[(row_num, col_num)] = int(cell_char)
+                elif cell_char == 'P':
+                    self.portals.append((row_num, col_num))
+        
+        
     def starting_nodes(self):
-        return [(r, c, fuel) for (r, c), fuel in self.agents.items()]
-    
+        result = []
+        for (row_num, col_num), fuel in self.agents.items():
+            result.append((row_num, col_num, fuel))
+        return result
+
     def is_goal(self, node):
-        row, col, _ = node
-        return (row, col) in self.goals
+        row_num, col_num, _ = node #dont need last val
+        return (row_num, col_num) in self.goals # return bool if in goal set 
     
     def outgoing_arcs(self, tail_node):
-        row, col, fuel = tail_node
+        row_num, col_num, fuel = tail_node
         arcs = []
-
-        # would need to adjust this depending on size of graph
+        
+        #(direction,x,y,cost)
         movements = [
             ('N', -1, 0, 5), ('NE', -1, 1, 7), ('E', 0, 1, 5), ('SE', 1, 1, 7),
             ('S', 1, 0, 5), ('SW', 1, -1, 7), ('W', 0, -1, 5), ('NW', -1, -1, 7)
         ]
         
-        for action, dr, dc, cost in movements:
-            new_row, new_col = row + dr, col + dc
-            if self.map[new_row][new_col] not in '+-|X' and fuel > 0:
+        for action, row_pos, col_pos, cost in movements:
+            new_row, new_col = row_num + row_pos, col_num + col_pos
+            #loops through movements until it finds a valid one 
+            if self.map[new_row][new_col] not in '+-|X' and fuel > 0: 
                 new_fuel = fuel - 1
                 arcs.append(Arc(tail_node, (new_row, new_col, new_fuel), action, cost))
         
-        if self.map[row][col] == 'F' and fuel < 9:
-            arcs.append(Arc(tail_node, (row, col, 9), 'Fuel up', 15))
+        if self.map[row_num][col_num] == 'F' and fuel < 9:
+            arcs.append(Arc(tail_node, (row_num, col_num, 9), 'Fuel up', 15))
         
-        if self.map[row][col] == 'P':
-            for pr, pc in self.portals:
-                if (pr, pc) != (row, col):
-                    arcs.append(Arc(tail_node, (pr, pc, fuel), f'Teleport to ({pr}, {pc})', 10))
+        if self.map[row_num][col_num] == 'P':
+            for port_posr, port_posc in self.portals:
+                if (port_posr, port_posc) != (row_num, col_num):
+                    arcs.append(Arc(tail_node, (port_posr, port_posc, fuel), f'Teleport to ({port_posr}, {port_posc})', 10))
         
         return arcs
+
+
+
+
